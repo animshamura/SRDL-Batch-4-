e866780b25a0d01091c13a1b8e7fea8a7ea96130
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2024-02-23 20:06:30 +0600

    Add files via upload

---
+++ Class No. 11/table-creation.sql
@@ -0,0 +1,14 @@
+create table skill (
+skill_name varchar(255),
+best_skill varchar(255)
+);
+
+create table subject (
+subject_name varchar(255),
+best_subject varchar(255)
+); 
+
+create table manage (
+genre_of_issues varchar(255),
+major_issue varchar(255)
+)
